45d17a7498622b0e8691e2d68bdb583623b54621
shashant <shashantyashu2004@gmail.com>
2025-03-20 17:31:24 +0530

    Updated files4

--- src/components/Holdings.js
+++ src/components/Holdings.js
@@ -8,7 +8,7 @@ const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3002/allHoldings").then((res) => {
+    axios.get("https://zerodha-clone-backend2.onrender.com/allHoldings").then((res) => { //http://localhost:3002
       // console.log(res.data);
       setAllHoldings(res.data);
     });
